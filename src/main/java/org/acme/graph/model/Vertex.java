896f9f2939e79d7bbe1ad0894cd87fd3154bbaeb
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2023-12-18 13:05:49 +0100

    Add files via upload

---
+++ src/main/java/org/acme/graph/model/Vertex.java
@@ -0,0 +1,89 @@
+package org.acme.graph.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.locationtech.jts.geom.Coordinate;
+
+/**
+ * 
+ * Un sommet dans un graphe
+ * 
+ * @author MBorne
+ *
+ */
+public class Vertex {
+
+	/**
+	 * Identifiant du sommet
+	 */
+	private String id;
+
+	/**
+	 * Position du sommet
+	 */
+	private Coordinate coordinate;
+
+	/**
+	 * dijkstra - coût pour atteindre le sommet
+	 */
+	private double cost;
+	/**
+	 * dijkstra - arc entrant avec le meilleur coût
+	 */
+	private Edge reachingEdge;
+	/**
+	 * dijkstra - indique si le sommet est visité
+	 */
+	private boolean visited;
+
+	public Vertex() {
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Coordinate getCoordinate() {
+		return coordinate;
+	}
+
+	public void setCoordinate(Coordinate coordinate) {
+		this.coordinate = coordinate;
+	}
+
+	@JsonIgnore
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	@JsonIgnore
+	public Edge getReachingEdge() {
+		return reachingEdge;
+	}
+
+	public void setReachingEdge(Edge reachingEdge) {
+		this.reachingEdge = reachingEdge;
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	@Override
+	public String toString() {
+		return id;
+	}
+
+}
---
+++ src/test/java/org/acme/graph/TestGraphFactory.java
@@ -0,0 +1,30 @@
+package org.acme.graph;
+
+import org.acme.graph.model.Edge;
+import org.acme.graph.model.Graph;
+import org.acme.graph.model.Vertex;
+import org.locationtech.jts.geom.Coordinate;
+
+public class TestGraphFactory {
+
+	/**
+	 * d / / a--b--c
+	 * 
+	 * @return
+	 */
+	public static Graph createGraph01() {
+		Graph graph = new Graph();
+
+		Vertex a = graph.createVertex(new Coordinate(0.0, 0.0), "a");
+		Vertex b = graph.createVertex(new Coordinate(1.0, 0.0), "b");
+		Vertex c = graph.createVertex(new Coordinate(2.0, 0.0), "c");
+		Vertex d = graph.createVertex(new Coordinate(1.0, 1.0), "d");
+		
+		Edge ab = graph.createEdge(a, b, "ab");
+		Edge bc = graph.createEdge(b, c, "ac");
+		Edge ad = graph.createEdge(a, d, "ad");
+		
+		return graph;
+	}
+
+}
